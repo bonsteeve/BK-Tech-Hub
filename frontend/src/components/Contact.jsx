bf7463381d35b07f075e78fa3b440acd20ee1075
Bonface Kioko <bonsteeve@gmail.com>
2026-01-24 02:27:21 +0300

    update jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -7,7 +7,8 @@ import { useToast } from '../hooks/use-toast';
 import { companyInfo } from '../mock';
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || window.location.origin;
 const API = `${BACKEND_URL}/api`;
 
 const Contact = () => {
